a40f045174067919ce8d386ea7021099308bea7d
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-11-24 00:16:17 +0530

    learn about clousers and settimeout functions

---
+++ setTimeout+clousers/index.js
@@ -0,0 +1,32 @@
+
+
+// function x() {
+//     for(let i=1; i<=5; i++) {
+//             setTimeout(function () {
+//         console.log(i);    
+//     }, i * 300);
+//     }
+    
+
+
+//     console.log("Namaste JavaScript");
+// }
+
+// x()
+
+
+
+function x() {
+    for(var i=1; i<=5; i++) {
+        function close(x) {
+            setTimeout(function () {
+                console.log(x);
+            }, x * 1000);
+        }
+        close(i);
+    }
+
+    console.log("Namaste Javascript");
+}
+
+x();
