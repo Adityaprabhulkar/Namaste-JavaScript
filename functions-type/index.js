8d43875784c2d3626e9f8bcc3edcff38e99bb99b
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-12-08 18:06:41 +0530

    functions types

---
+++ functions-type/index.js
@@ -0,0 +1,33 @@
+//Function Statement
+function a() {
+    console.log("a called");
+}
+
+// Function Expression
+const b = function (param1) {
+    return function xyz() {
+
+    }
+}
+
+console.log(b());
+
+
+// Anonymous Function
+// function () {
+
+// }
+
+// Named function Expression
+
+//Difference bet'n Parameters and Arguments
+// Parameters => which passing variable inside function called parameter
+
+// passing value inside function called as Arguments
+
+
+// First Class function 
+// Ability to use Value inside function or another function and return this function inside the function called as first called function
+
+//Arrow function 
+// is part of Es6 ecma script
